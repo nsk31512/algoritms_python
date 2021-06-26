3dcfdbd2f1640405fa08f93e618cbefbc28c468d
nsk31512 <kuznetsov_va92@bk.ru>
2021-06-26 12:47:26 +0700

    Исправил первую задачу, т.к. изначально сделал сортировку по возрастанию, а не по убыванию

--- task7-1.py
+++ task7-1.py
@@ -17,7 +17,7 @@ print(my_list)
 def bubble_sort(list):    
     for j in range(1, len(list)):
         for i in range(len(list) - 1):
-            if list[i] > list[i + 1]:
+            if list[i] < list[i + 1]:
                 list[i], list[i + 1] = list[i + 1], list[i]
 
 
